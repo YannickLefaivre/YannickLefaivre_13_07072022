1abd5c354527256237dd0081d80dfed9d03186ac
Yannick LEFAIVRE <debeug@outlook.fr>
2022-07-21 09:11:08 -0400

    feat: add the section detailing the features of the company's services

---
+++ src/__mocks__/features.js
@@ -0,0 +1,27 @@
+import chatIcon from "../features/authentication/assets/icon-chat.png"
+import moneyIcon from "../features/authentication/assets/icon-money.png"
+import securityIcon from "../features/authentication/assets/icon-security.png"
+
+export const mockedFeatures = [
+  {
+    icon: chatIcon,
+    iconAltText: "Chat Icon",
+    title: "You are our #1 priority",
+    description:
+      "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+  },
+  {
+    icon: moneyIcon,
+    iconAltText: "Money Icon",
+    title: "More savings means higher rates",
+    description:
+      "The more you save with us, the higher your interest rate will be!",
+  },
+  {
+    icon: securityIcon,
+    iconAltText: "Security Icon",
+    title: "Security you can trust",
+    description:
+      "We use top of the line encryption to make sure your data and money is always safe.",
+  },
+]

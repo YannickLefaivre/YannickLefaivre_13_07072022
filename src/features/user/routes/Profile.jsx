8abf60ea7ceb7af9a074ee7b3cbaeb6f5948a21e
Yannick LEFAIVRE <debeug@outlook.fr>
2022-07-28 10:33:29 -0400

    fix: prop name className

--- src/features/user/routes/Profile.jsx
+++ src/features/user/routes/Profile.jsx
@@ -65,50 +65,50 @@ function Profile() {
           <main className="main bg-dark">
             <MaintContentHeader />
             <h2 className="sr-only">Accounts</h2>
-            <section class="account">
-              <div class="account-content-wrapper">
-                <h3 class="account-title">
+            <section className="account">
+              <div className="account-content-wrapper">
+                <h3 className="account-title">
                   Argent Bank Checking (x8349)
                 </h3>
-                <p class="account-amount">$2,082.79</p>
-                <p class="account-amount-description">
+                <p className="account-amount">$2,082.79</p>
+                <p className="account-amount-description">
                   Available Balance
                 </p>
               </div>
-              <div class="account-content-wrapper cta">
-                <button class="transaction-button">
+              <div className="account-content-wrapper cta">
+                <button className="transaction-button">
                   View transactions
                 </button>
               </div>
             </section>
-            <section class="account">
-              <div class="account-content-wrapper">
-                <h3 class="account-title">
+            <section className="account">
+              <div className="account-content-wrapper">
+                <h3 className="account-title">
                   Argent Bank Savings (x6712)
                 </h3>
-                <p class="account-amount">$10,928.42</p>
-                <p class="account-amount-description">
+                <p className="account-amount">$10,928.42</p>
+                <p className="account-amount-description">
                   Available Balance
                 </p>
               </div>
-              <div class="account-content-wrapper cta">
-                <button class="transaction-button">
+              <div className="account-content-wrapper cta">
+                <button className="transaction-button">
                   View transactions
                 </button>
               </div>
             </section>
-            <section class="account">
-              <div class="account-content-wrapper">
-                <h3 class="account-title">
+            <section className="account">
+              <div className="account-content-wrapper">
+                <h3 className="account-title">
                   Argent Bank Credit Card (x8349)
                 </h3>
-                <p class="account-amount">$184.30</p>
-                <p class="account-amount-description">
+                <p className="account-amount">$184.30</p>
+                <p className="account-amount-description">
                   Current Balance
                 </p>
               </div>
-              <div class="account-content-wrapper cta">
-                <button class="transaction-button">
+              <div className="account-content-wrapper cta">
+                <button className="transaction-button">
                   View transactions
                 </button>
               </div>
